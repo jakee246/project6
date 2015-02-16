a0676619e98671ad264dcfb7b85b45c781ecf036
jakee246 <Jake@Jacobs-MacBook-Pro-2.local>
2015-02-16 00:43:02 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+require "sinatra/content_for"
+
+class MyWebApp < Sinatra::Base
+	helpers Sinatra::ContentFor
+  get '/' do
+  	last_modified Time.now
+  	erb :"index#{ rand(3) + 1}"
+   
+  end
+end
